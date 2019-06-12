2af422664af2395a6bca97275d64df88d5d1d098
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2019-06-13 07:43:00 +0900

    Show loader when fetching firebase result

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
-import { Container, Header, Button, Form } from "semantic-ui-react";
+import {
+  Container,
+  Dimmer,
+  Loader,
+  Header,
+  Button,
+  Form
+} from "semantic-ui-react";
 import "./Login.css";
 import Auth from "../auth/Auth";
 
 interface Props {
   auth: Auth;
 }
+interface State {
+  loading: boolean;
+}
 
-class Login extends Component<Props, {}> {
+class Login extends Component<Props, State> {
+  readonly state: State = {
+    loading: true
+  };
+
   componentDidMount() {
     this.doLogin();
   }
@@ -17,19 +31,25 @@ class Login extends Component<Props, {}> {
   }
 
   doLogin() {
-    // TODO: show loader
     this.props.auth.firebase.auth.getRedirectResult().then(authResult => {
       if (authResult.user != null) {
         this.props.auth.handleAuthentication(authResult);
+      } else {
+        this.setState({ loading: false });
       }
     });
   }
 
   render() {
+    let { loading } = this.state;
+
     return (
       <Container text className="Login-container">
         <Header as="h2" icon textAlign="center" color="grey">
           <Header.Content>NotePage</Header.Content>
+          <Dimmer active={loading}>
+            <Loader inverted>Loading...</Loader>
+          </Dimmer>
           <Form className="Login-form">
             <Button
               id="qsLoginBtn"
