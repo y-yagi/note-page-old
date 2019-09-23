ed68c7d2a4a20d8b0f342f6b967750cda637633b
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2019-09-23 13:46:38 +0900

    Use React Hook in PageForm

--- src/components/PageForm.tsx
+++ src/components/PageForm.tsx
@@ -1,5 +1,5 @@
 import Auth from "../auth/Auth";
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 import { Button, Form } from "semantic-ui-react";
 
@@ -7,102 +7,89 @@ interface Props {
   auth: Auth;
   pageID: string;
 }
-interface State {
-  name: string;
-  content: string;
-  action: string;
-}
 
-class PageForm extends Component<Props, State> {
-  readonly state: State = {
-    name: "",
-    content: "",
-    action: "create"
-  };
+function PageForm(props: Props) {
+  const [name, setName] = useState("");
+  const [content, setContent] = useState("");
+  const [action, setAction] = useState("create");
 
-  constructor(props) {
-    super(props);
+  useEffect(() => {
+    fetchPage(props.pageID);
+    // eslint-disable-next-line
+  }, [props.pageID]);
 
-    this.handleChangeName = this.handleChangeName.bind(this);
-    this.handleChangeContent = this.handleChangeContent.bind(this);
-    this.onSubmitPage = this.onSubmitPage.bind(this);
-    this.scrollToBottom = this.scrollToBottom.bind(this);
-  }
-
-  handleChangeName(event) {
-    this.setState({ name: event.target.value });
-  }
-
-  handleChangeContent(event) {
-    this.setState({ content: event.target.value });
-  }
-
-  scrollToBottom(_) {
-    window.scrollTo(0, document.body.scrollHeight);
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.pageID !== "" && this.props.pageID !== prevProps.pageID) {
-      this.props.auth.firebase
-        .page(this.props.pageID)
+  async function fetchPage(id: string) {
+    if (id !== "") {
+      props.auth.firebase
+        .page(id)
         .get()
         .then(snapshot => {
           const page = snapshot.data();
-          this.setState({
-            name: page.name,
-            content: page.content,
-            action: "update"
-          });
+
+          setName(page.name);
+          setContent(page.content);
+          setAction("update");
         });
     }
   }
 
-  onSubmitPage = event => {
+  function handleChangeName(event) {
+    setName(event.target.value);
+  }
+
+  function handleChangeContent(event) {
+    setContent(event.target.value);
+  }
+
+  function scrollToBottom(_) {
+    window.scrollTo(0, document.body.scrollHeight);
+  }
+
+  function onSubmitPage(event) {
     const data = {
-      name: this.state.name,
-      content: this.state.content,
-      userId: this.props.auth.userID(),
-      createdAt: this.props.auth.firebase.timestamp()
+      name: name,
+      content: content,
+      userId: props.auth.userID(),
+      createdAt: props.auth.firebase.timestamp()
     };
 
-    if (this.props.pageID === "") {
-      this.props.auth.firebase.pages().add(data);
+    if (props.pageID === "") {
+      props.auth.firebase.pages().add(data);
     } else {
-      this.props.auth.firebase.page(this.props.pageID).set(data);
+      props.auth.firebase.page(props.pageID).set(data);
     }
 
-    this.setState({ name: "", content: "", action: "create" });
+    setName("");
+    setContent("");
+    setAction("create");
+
     event.preventDefault();
-  };
-
-  render() {
-    const { name, content, action } = this.state;
-
-    return (
-      <Form onSubmit={event => this.onSubmitPage(event)}>
-        <Form.Field required>
-          <label>Page Name</label>
-          <input
-            placeholder="Name"
-            required
-            value={name}
-            onChange={this.handleChangeName}
-          />
-        </Form.Field>
-        <Form.Field required>
-          <label>Content</label>
-          <TextareaAutosize
-            placeholder="Content"
-            required
-            value={content}
-            onChange={this.handleChangeContent}
-            onHeightChange={this.scrollToBottom}
-          />
-        </Form.Field>
-        <Button type="submit">{action}</Button>
-      </Form>
-    );
   }
+
+  return (
+    <Form onSubmit={event => onSubmitPage(event)}>
+      <Form.Field required>
+        <label>Page Name</label>
+        <input
+          placeholder="Name"
+          required
+          value={name}
+          onChange={handleChangeName}
+        />
+      </Form.Field>
+      <Form.Field required>
+        <label>Content</label>
+        <TextareaAutosize
+          placeholder="Content"
+          required
+          value={content}
+          onChange={handleChangeContent}
+          onHeightChange={scrollToBottom}
+        />
+      </Form.Field>
+      <Button type="submit">{action}</Button>
+    </Form>
+  );
 }
 
 export default PageForm;
