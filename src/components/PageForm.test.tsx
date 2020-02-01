a1eb4f8085e09ac3c0bf334a6ce7483d6f2ffcac
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2020-02-01 14:44:04 +0900

    Add `@testing-library/react` and `@testing-library/jest-dom`

--- src/components/PageForm.test.tsx
+++ src/components/PageForm.test.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import PageForm from "./PageForm";
 import * as firebase from "@firebase/testing";
 import Auth from "../auth/Auth";
+import { render } from "@testing-library/react";
 
 const FIRESTORE_PROJECT_ID = "my-test-project";
 
@@ -11,11 +12,17 @@ beforeEach(() => {
     projectId: FIRESTORE_PROJECT_ID,
     auth: { uid: "alice", email: "alice@example.com" }
   });
-})
+});
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   const auth = new Auth();
   ReactDOM.render(<PageForm auth={auth} firebase={firebase} />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders welcome message", () => {
+  const auth = new Auth();
+  const { getByText } = render(<PageForm auth={auth} firebase={firebase} />);
+  expect(getByText("Learn React")).toBeInTheDocument();
 });
