cb639b54ee3faf2f550195d854c8b06e372b27e7
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2020-07-04 14:33:58 +0900

    Remove unused Link

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
 import "./App.css";
 import Interweave from "interweave";
 import { UrlMatcher } from "interweave-autolink";
